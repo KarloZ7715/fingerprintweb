09426835f3a83edc6a4bb2592472bc521964f183
Bara34r <baraujovega41@correo.unicordoba.edu.co>
2025-11-06 19:21:30 -0500

    feat: Añade modelo alarma

---
+++ app/Models/Alarma.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Alarma  extends Model
+{
+    protected $table = 'alarma';
+
+    protected $fillable = [
+         'id',
+        'nombre',
+        'estado',
+        'duracion',
+        'h_encendido',
+        'h_apagado',
+        'sucursal_id',
+        'updated_at'
+    ];
+
+
+    public function sucursal()
+{
+    return $this->belongsTo(Sucursal::class);
+}
+}
