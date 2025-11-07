9afb3651ce6606a8f7477f114cbdf5f4fef4cfb0
Bara34r <baraujovega41@correo.unicordoba.edu.co>
2025-11-07 11:04:23 -0500

    feat: Se añadio un campo en la tabla para apagar y enceder la alarma

--- app/Filament/Resources/Alarmas/Tables/AlarmasTable.php
+++ app/Filament/Resources/Alarmas/Tables/AlarmasTable.php
@@ -8,7 +8,8 @@ use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-
+use Filament\Notifications\Notification;
+use Filament\Actions\Action;
 class AlarmasTable
 {
     public static function configure(Table $table): Table
@@ -16,8 +17,8 @@ class AlarmasTable
         return $table
             ->columns([
                 TextColumn::make('nombre')
-                      ->searchable()
-                      ->label('Nombre alarma'),
+                    ->searchable()
+                    ->label('Nombre alarma'),
                 TextColumn::make('estado')
                     ->searchable(),
                 TextColumn::make('duracion')
@@ -32,10 +33,10 @@ class AlarmasTable
                     ->time()
                     ->sortable()
                     ->label('Hora apagado'),
-               TextColumn::make('sucursal.nombre')
-    ->label('Sucursal asignada')
-    ->sortable()
-    ->searchable(),
+                TextColumn::make('sucursal.nombre')
+                    ->label('Sucursal asignada')
+                    ->sortable()
+                    ->searchable(),
 
                 TextColumn::make('created_at')
                     ->dateTime()
@@ -48,6 +49,28 @@ class AlarmasTable
             ->recordActions([
                 ViewAction::make(),
                 EditAction::make(),
+                Action::make('accionar')
+                    ->label(fn ($record) => $record->estado === 'Activa' ? 'Apagar' : 'Encender')
+                    ->color(fn ($record) => $record->estado === 'Activa' ? 'danger' : 'success')
+                    ->requiresConfirmation()
+                    ->modalHeading(fn ($record) => $record->estado === 'Activa'
+                        ? '¿Desea apagar la alarma?'
+                        : '¿Desea encender la alarma?'
+                    )
+                    ->action(function ($record) {
+                        $nuevoEstado = $record->estado === 'Activa' ? 'Inactiva' : 'Activa';
+                        $record->estado = $nuevoEstado;
+                        $record->save();
+
+                        Notification::make()
+                            ->success()
+                            ->title(
+                                $nuevoEstado === 'Activa'
+                                    ? 'Alarma encendida correctamente'
+                                    : 'Alarma apagada correctamente'
+                            )
+                            ->send();
+                    }),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
