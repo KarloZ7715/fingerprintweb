3f714dde634207215256479d134b78f78f9a8ef4
Bara34r <baraujovega41@correo.unicordoba.edu.co>
2025-11-07 11:01:02 -0500

    feat: agregar rutas API de alarmas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/prueba', function () {
+    return 'funciona la api!';
+});
+use App\Http\Controllers\Api\AlarmaController;
+use App\Http\Controllers\Api\EventoController;
+
+Route::get('/alarma/{id}/activar', [AlarmaController::class, 'activar']); //Enceder alarma
+Route::get('/alarma/{id}/desactivar', [AlarmaController::class, 'desactivar']); //apagar alarma
+Route::get('/eventos', [EventoController::class, 'index']);
+
+Route::post('/evento', [EventoController::class, 'store']);
+
+Route::get('/prueba', function () {
+    return 'funciona la api!';
+});
