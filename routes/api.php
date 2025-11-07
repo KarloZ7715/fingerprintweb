a12d08826c53dab0c6081d984194821abb938424
Bara34r <baraujovega41@correo.unicordoba.edu.co>
2025-11-07 11:02:36 -0500

    feat:  se configurara para las rutas de las api (no sirve)

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AlarmaController;
+use App\Http\Controllers\Api\EventoController;
+
+Route::post('/alarma/{id}/activar', [AlarmaController::class, 'activar']);
+Route::post('/alarma/{id}/desactivar', [AlarmaController::class, 'desactivar']);
+Route::get('/eventos', [EventoController::class, 'index']);
+
+Route::post('/evento', [EventoController::class, 'store']);
+
+Route::get('/prueba', function () {
+    return 'funciona la api!';
+});
