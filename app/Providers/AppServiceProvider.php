0b58ce0fdef73ed6eea60e373ac778fec0fc4e7a
KarloZ7715 <ccanabalcordero70@correo.unicordoba.edu.co>
2025-11-08 00:42:03 -0500

    chore(config): configurar zona horaria y locale de Colombia en toda la aplicación
    - Cambiar timezone a 'America/Bogota' (UTC-5) en config/app.php
    - Establecer locale a 'es' (español) con faker es_CO
    - Añadir timezone offset -05:00 en conexiones MySQL y MariaDB
    - Configurar Carbon en AppServiceProvider con setLocale('es')
    - Sincronizar zona horaria en todo el stack: PHP, Laravel, Base de datos
    - Garantiza que todos los timestamps se manejen en hora de Colombia

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Date;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Carbon::setLocale('es');
+        date_default_timezone_set('America/Bogota');
+
+
+        Date::use(Carbon::class);
     }
 }
