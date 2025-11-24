057d422b71d6594d4f027ff4178812606b36b9da
Jhon Quiceno <quicenojale@gmail.com>
2025-11-24 14:51:23 -0500

    feat: reemplazar FilamentInfoWidget por ManualUsuarioWidget en panel admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,7 +11,6 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -20,6 +19,7 @@ use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Filament\Navigation\NavigationItem;
 use App\Filament\Pages\Auth\CustomRequestPasswordReset;
+use App\Filament\Widgets\ManualUsuarioWidget;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -44,7 +44,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 AccountWidget::class,
-                FilamentInfoWidget::class,
+                ManualUsuarioWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
