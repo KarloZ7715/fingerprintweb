c391531b25c9ecbc3809168e64c2e2741b2842ef
KarloZ7715 <ccanabalcordero70@correo.unicordoba.edu.co>
2025-11-23 19:28:41 -0500

    fix: redirigir ruta raíz al panel de administración
    - Cambia página de bienvenida por redirect a /admin

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
+
 Route::get('/prueba', function () {
     return 'funciona la api!';
 });
